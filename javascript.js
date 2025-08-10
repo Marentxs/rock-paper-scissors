e105cf084f69bb2de770f08dcec06450df2eb7fd
Julian <111262742+Marentxs@users.noreply.github.com>
2025-08-09 19:48:36 -0500

    Add logic for 1 round and score variables

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-console.log("JavaScript file linked successfully!");
-
 function getComputerChoice() {
   //Assign each option with a random number between 0-1 with decimals//
   let random1 = Math.random();
@@ -18,10 +16,8 @@ function getComputerChoice() {
 
   //Print in the console the winner of the random choice//
   console.log(computer);
+  return computer;
 }
-
-//Call function so that it runs//
-getComputerChoice();
 
 function getHumanChoice() {
   //Asks hand to user//
@@ -38,9 +34,66 @@ function getHumanChoice() {
     user = "User plays scissors";
   }
 
-  //Saves answer in the console//
+  //Prints selection in the console//
   console.log(user);
+  return user;
 }
 
-//Call function so that it runs//
-getHumanChoice();
+//Logic to play 1 round//
+function playRound(humanChoice, computerChoice) {
+  if (
+    humanChoice === "User plays rock" &&
+    computerChoice === "Computer plays scissors"
+  ) {
+    return "You win! rock wins against scissors";
+  } else if (
+    humanChoice === "User plays rock" &&
+    computerChoice === "Computer plays paper"
+  ) {
+    return "The computer wins, rock loses against paper";
+  } else if (
+    humanChoice === "User plays paper" &&
+    computerChoice === "Computer plays rock"
+  ) {
+    return "You win! paper wins against rock";
+  } else if (
+    humanChoice === "User plays paper" &&
+    computerChoice === "Computer plays scissors"
+  ) {
+    return "The computer wins, paper loses against scissors";
+  } else if (
+    humanChoice === "User plays scissors" &&
+    computerChoice === "Computer plays paper"
+  ) {
+    return "You win! scissors wins against paper";
+  } else if (
+    humanChoice === "User plays scissors" &&
+    computerChoice === "Computer plays rock"
+  ) {
+    return "The computer wins, scissors loses against rock";
+  } else {
+    return "It's a tie!";
+  }
+}
+
+//Saves results from functions into variables//
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+const result = playRound(humanSelection, computerSelection);
+
+console.log(result);
+
+//Variables for score
+let humanScore = 0;
+let computerScore = 0;
+
+//Point system//
+if (result.includes("You win")) {
+  humanScore += 100;
+} else if (result.includes("The computer wins")) {
+  computerScore += 100;
+} else {
+}
+
+//Keeps score//
+console.log(`Score: Human ${humanScore} - Computer ${computerScore}`);
